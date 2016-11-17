e0143b994a4fabd09fc6d1fc690b65af992ee8a3
Daniel Gardner <bowshanks@gmail.com>
2016-11-16 20:26:51 -0700

    adjusting tooltips/menu

--- js/parallax.js
+++ js/parallax.js
@@ -34,13 +34,14 @@ $(window).scroll(function(){
       $('.menu-holder').css({'visibility':'hidden'});
     });
     //////////////////////////////////////////////////
-    $('.attribution').on('click',function(){
-      $('.tooltiptext').css({'visibility':'visible'});
-    });
-
-    $('.main-container').not('.attribution').on('click',function(){
-      $('.menu-holder').css({'visibility':'hidden'});
-    });
+    // $('.attribution').on('click',function(){
+    //   $('.tooltiptext').css({'visibility':'visible'});
+    // });
+    //
+    // $('body').not('.attribution').on('click',function(){
+    //   console.log('hello');
+    //   $('.tooltiptext').css({'visibility':'hidden'});
+    // });
 
       // <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
       // <svg version="1.1" xmlns="http://www.w3.org/2000/svg" height="560" width="588">
