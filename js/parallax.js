b4b77cdc0ba4d8a04da0f73f8ef22dc859596e2a
Daniel Gardner <bowshanks@gmail.com>
2016-11-16 20:01:37 -0700

    improved menu, fixed svg errors

--- js/parallax.js
+++ js/parallax.js
@@ -26,6 +26,13 @@ $(window).scroll(function(){
         'transform': 'translate(0px, '+ winScroll /40 +'%)'
       });
 
+    $('.menu-button').on('click',function(){
+      $('.menu-holder').css({'visibility':'visible'});
+    });
+
+    $('.main-container, .menu-holder').on('click',function(){
+      $('.menu-holder').css({'visibility':'hidden'});
+    });
 
       // <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
       // <svg version="1.1" xmlns="http://www.w3.org/2000/svg" height="560" width="588">
